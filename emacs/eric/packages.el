29623818d22ac4dbf3aaa257daeefb144dbdb9ea
Eric Siegel <siegel.eric@gmail.com>
2015-02-02 16:41:26 -0800

    fix spacemacs package load order

--- emacs/eric/packages.el
+++ emacs/eric/packages.el
@@ -5,16 +5,16 @@
                         go-mode
                         golint
                         magit 
-                        rainbow-mode))
+                        rainbow-mode
+                        rust-mode))
 
-(defun eric/init-company-ghc()    (use-package company-ghc))
-(defun eric/init-emmet-mode()     (use-package emmet-mode))
-(defun eric/init-magit()          (use-package magit))
-(defun eric/init-rainbow-mode()   (use-package rainbow-mode))
-
-;; go packages
-(defun eric/init-go-mode() (use-package go-mode))
-(defun eric/init-golint()  (use-package golint))
+(defun eric/init-company-ghc()  (use-package company-ghc))
+(defun eric/init-emmet-mode()   (use-package emmet-mode))
+(defun eric/init-magit()        (use-package magit))
+(defun eric/init-rainbow-mode() (use-package rainbow-mode))
+(defun eric/init-rust-mode()    (use-package rust-mode))
+(defun eric/init-go-mode()      (use-package go-mode))
+(defun eric/init-golint()       (use-package golint))
 (defun eric/init-company-go()
-  ; company-go by default will load before go-mode is loaded.
+  ; must load after go-mode
   (eval-after-load 'go-mode '(require 'company-go)))
