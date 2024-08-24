3a185d3c92eb3c4bda375e9afcf69db8f4e1bc90
Eric Siegel <eric.siegel@10xgenomics.com>
2024-08-23 19:24:04 -0700

    More nvim

--- nvim/lua/plugins/files.lua
+++ nvim/lua/plugins/files.lua
@@ -3,34 +3,60 @@
 -- "<cmd>NvimTreeFindFileToggle!<cr>",
 
 return {
-	-- nvim-tree
-	{
-		"nvim-tree/nvim-tree.lua",
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-		keys = {
-			{ "<leader>n", "<cmd>NvimTreeToggle<cr>", desc = "NvimTree Toggle" },
-			{
-				"<C-d>",
-				function()
-					local api = require("nvim-tree.api")
-					api.tree.open({ path = nil, find_file = true, update_root = true })
-					api.tree.focus()
-				end,
-				desc = "NvimTree Toggle open file",
-			},
-		},
-		config = function()
-			require("nvim-tree").setup({
-				actions = {
-					open_file = {
-						window_picker = {
-							enable = false,
-						},
-					},
-				},
-			})
-		end,
-	},
+  -- nvim-tree
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    keys = {
+      { "<leader>n", "<cmd>NvimTreeToggle<cr>", desc = "NvimTree Toggle" },
+      {
+        "<C-d>",
+        function()
+          local api = require("nvim-tree.api")
+          api.tree.open({ path = nil, find_file = true, update_root = true })
+          api.tree.focus()
+        end,
+        desc = "NvimTree Toggle open file",
+      },
+    },
+    config = function()
+      require("nvim-tree").setup({
+        actions = {
+          open_file = {
+            window_picker = {
+              enable = false,
+            },
+          },
+        },
+      })
+    end,
+  },
+
+  --calltree--
+  {
+    'ldelossa/litee.nvim',
+    event = "VeryLazy",
+    opts = {
+      notify = { enabled = false },
+      panel = {
+        orientation = "bottom",
+        panel_size = 10,
+      },
+    },
+    config = function(_, opts) require('litee.lib').setup(opts) end
+  },
+
+  {
+    'ldelossa/litee-calltree.nvim',
+    dependencies = 'ldelossa/litee.nvim',
+    event = "VeryLazy",
+    opts = {
+      on_open = "panel",
+      map_resize_keys = false,
+    },
+    config = function(_, opts) require('litee.calltree').setup(opts) end
+  },
+
 }
--- nvim/lua/plugins/lsp/servers.lua
+++ nvim/lua/plugins/lsp/servers.lua
@@ -3,36 +3,42 @@
 -- Add any servers here together with their settings
 ---@type lspconfig.options
 local servers = {
-	bashls = {},
-	clangd = {},
-	cssls = {},
-	eslint = {
-		workingDirectory = { mode = "auto" },
-	},
-	gopls = {},
-	tsserver = {},
-	html = {},
-	jsonls = {},
-	pyright = {},
-	yamlls = {},
-	lua_ls = {
-		settings = {
-			Lua = {
-				diagnostics = {
-					globals = { "vim" },
-				},
-				workspace = {
-					checkThirdParty = false,
-					library = vim.api.nvim_get_runtime_file("", true),
-				},
-				completion = {
-					callSnippet = "Replace",
-				},
-			},
-		},
-	},
+  bashls = {},
+  clangd = {},
+  cssls = {},
+  eslint = {
+    workingDirectory = { mode = "auto" },
+  },
+  gopls = {},
+  tsserver = {},
+  html = {},
+  jsonls = {},
+  yamlls = {},
 
-	rust_analyzer = {},
+  -- lua
+  lua_ls = {
+    settings = {
+      Lua = {
+        diagnostics = {
+          globals = { "vim" },
+        },
+        workspace = {
+          checkThirdParty = false,
+          library = vim.api.nvim_get_runtime_file("", true),
+        },
+        completion = {
+          callSnippet = "Replace",
+        },
+      },
+    },
+  },
+
+  -- rust
+  rust_analyzer = {},
+
+  -- python
+  pyright = {},
+  ruff = {},
 }
 
 return servers
