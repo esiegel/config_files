8e03e17d5d46f4e1c9eb8f0fdff18c29799b87dd
Eric Siegel <siegel.eric@gmail.com>
2024-08-23 19:24:04 -0700

    More nvim

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -1,45 +1,45 @@
 return {
-	"folke/trouble.nvim",
-	opts = {},
-	cmd = "Trouble",
-	keys = {
-		{
-			"<leader>xx",
-			"<cmd>Trouble diagnostics toggle<cr>",
-			desc = "Diagnostics (Trouble)",
-		},
-		{
-			"<leader>xX",
-			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-			desc = "Buffer Diagnostics (Trouble)",
-		},
-		{
-			"<leader>cs",
-			"<cmd>Trouble symbols toggle focus=false<cr>",
-			desc = "Symbols (Trouble)",
-		},
-		{
-			"<leader>cl",
-			"<cmd>Trouble lsp toggle focus=true win.position=right win.size=80<cr>",
-			desc = "LSP Definitions / references / ... (Trouble)",
-		},
-		{
-			"<leader>xL",
-			"<cmd>Trouble loclist toggle<cr>",
-			desc = "Location List (Trouble)",
-		},
-		{
-			"<leader>xQ",
-			"<cmd>Trouble qflist toggle<cr>",
-			desc = "Quickfix List (Trouble)",
-		},
-	},
-	config = function()
-		local trouble = require("trouble")
+  "folke/trouble.nvim",
+  opts = {},
+  cmd = "Trouble",
+  keys = {
+    {
+      "<leader>xx",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    {
+      "<leader>xX",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+    {
+      "<leader>cs",
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Symbols (Trouble)",
+    },
+    {
+      "<leader>cl",
+      "<cmd>Trouble lsp toggle focus=true win.position=right win.size=80<cr>",
+      desc = "LSP Definitions / references / ... (Trouble)",
+    },
+    {
+      "<leader>xL",
+      "<cmd>Trouble loclist toggle<cr>",
+      desc = "Location List (Trouble)",
+    },
+    {
+      "<leader>xQ",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
+    },
+  },
+  config = function()
+    local trouble = require("trouble")
 
-		trouble.setup({
-			auto_preview = false, -- automatically open preview when on an item
-			auto_refresh = false, -- auto refresh when open
-		})
-	end,
+    trouble.setup({
+      auto_preview = false, -- automatically open preview when on an item
+      auto_refresh = false, -- auto refresh when open
+    })
+  end,
 }
