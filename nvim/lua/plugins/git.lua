03e0dd71800cb3e4f19f15bbda704c7e4974a5ac
Eric Siegel <eric.siegel@10xgenomics.com>
2024-09-03 14:46:34 -0700

    more lsp

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,23 +1,23 @@
 local shell = require("util.shell")
 
 local diff_main = function()
-	local mainbranch, err = shell.cmd("git mainbranch")
-	if err then
-		return
-	end
+  local mainbranch, err = shell.cmd("git mainbranch")
+  if err then
+    return
+  end
 
-	vim.cmd("Gdiff " .. mainbranch)
+  vim.cmd("Gdiff " .. mainbranch)
 end
 
 -- Git source control management
 return {
-	"tpope/vim-fugitive",
-	dependencies = {
-		"tpope/vim-rhubarb", -- for adding GBrowse support
-	},
-	lazy = false,
-	keys = {
-		{ "<leader>v", "<cmd>Gdiff<cr>", desc = "Git Diff file" },
-		{ "<leader>d", diff_main, desc = "Git Diff file with main" },
-	},
+  "tpope/vim-fugitive",
+  dependencies = {
+    "tpope/vim-rhubarb", -- for adding GBrowse support
+  },
+  lazy = false,
+  keys = {
+    { "<leader>v", "<cmd>Gdiff<cr>", desc = "Git Diff file" },
+    { "<leader>d", diff_main,        desc = "Git Diff file with main" },
+  },
 }
